7316fa02e94cad9bbd8e027a2e8f7c18145aeeff
vkharunzhyn <messuri.87@mail.ru>
2021-06-09 13:58:00 +0300

    gfjgfjfg

--- HomeWork04/src/Task01.java
+++ HomeWork04/src/Task01.java
@@ -1,11 +1,11 @@
 
 //Создать двумерный массив размером 5 на 5. Заполнить последовательно числами от 0 до 24.
 
-
 public class Task01 {
     public static void main(String[] args) {
         int[][] a = new int[5][5];
         int count = 0;
+        int gg = 100;
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[0].length; j++) {
                 a[i][j] = count;
